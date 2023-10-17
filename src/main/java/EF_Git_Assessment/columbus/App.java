6990be6d5029f284fd11ff1115301ad50107e154
Bevenkatesh <44633417+Bevenkatesh@users.noreply.github.com>
2023-10-17 09:36:53 -0400

    Add files via upload

---
+++ src/main/java/EF_Git_Assessment/columbus/App.java
@@ -0,0 +1,18 @@
+package EF_Git_Assessment.columbus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int first = 10;
+	    int second = 20;
+
+	    // add two numbers
+	    int sum = first + second;
+	    System.out.println(first + " + " + second + " = "  + sum);
+    }
+}
